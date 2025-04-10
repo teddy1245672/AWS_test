0197075f08d0d9be1b97a3a10df8823e05d31060
user <user@WINDOWS-R5U85RI.cdsafi.com>
2025-04-10 09:58:33 +0800

    123

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,11 +3,11 @@ echo "Starting Django application..."
 apt-get update
 apt-get install -y python3-pip
 pip3 install django whitenoise openpyxl djangorestframework
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source ~/.nvm/nvm.sh
-nvm install 12.16.1
-npm install vue@2.5.17
-cd /home/ubuntu/aws_test
-npm install
-npm run build
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+#source ~/.nvm/nvm.sh
+#nvm install 12.16.1
+#npm install vue@2.5.17
+cd /home/ubuntu/AWS_test
+#sudo npm install
+#npm run build
 python3 manage.py runserver 0.0.0.0:8000 &
